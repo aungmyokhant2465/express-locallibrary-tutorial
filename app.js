1f31e935d88d7b90d853050433520c3d68a563da
Aung Myo Khant <aungmyokhant2465@gmail.com>
2021-10-23 21:19:37 +0630

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose')
-var mongoDB = 'mongodb+srv://aungmk:aungmk@cluster0.3h6wj.mongodb.net/local_library?retryWrites=true&w=majority'
+var dev_db_url = 'mongodb+srv://aungmk:aungmk@cluster0.3h6wj.mongodb.net/local_library?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
